e21fc65a10de8e99c45f7c10709aa076fde9abd5
Shubham Bhadani <shubham.bhadani07@gmail.com>
2020-06-14 23:28:58 +0530

    productPage

--- src/Components/ProductPage/productPage.tsx
+++ src/Components/ProductPage/productPage.tsx
@@ -1,1 +1,14 @@
 
+import React, { Component } from 'react';
+
+import './productPage.scss'
+
+export default class Product extends Component {
+    static contextType = ShopContext;
+    render() {
+        return (
+           
+        )
+    }
+
+}
